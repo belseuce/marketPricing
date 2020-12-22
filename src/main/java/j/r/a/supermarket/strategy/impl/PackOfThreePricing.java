abb201fde65d74a044c897719964baa0ea9fd567
belseuce <rufin.assoumou@gmail.com>
2020-12-22 20:43:45 +0100

    fix pack of 3 Tests

--- src/main/java/j/r/a/supermarket/strategy/impl/PackOfThreePricing.java
+++ src/main/java/j/r/a/supermarket/strategy/impl/PackOfThreePricing.java
@@ -6,10 +6,12 @@ import j.r.a.supermarket.model.PricingPartition;
 import j.r.a.supermarket.model.Quantity;
 import j.r.a.supermarket.strategy.IPricingStrategy;
 import javafx.util.Pair;
+import lombok.EqualsAndHashCode;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
+@EqualsAndHashCode
 public class PackOfThreePricing implements IPricingStrategy {
     private static final String CODE = "CODE02";
     private static final Pair<Quantity, Price> DEFAULT_PRICE = new Pair<>(new Quantity(3), new Price(1.00));
@@ -26,6 +28,21 @@ public class PackOfThreePricing implements IPricingStrategy {
 
     @Override
     public List<PricingPartition> getPricingPartitions(Quantity quantity, Article article) {
-        return Collections.emptyList();
+
+        int nbPacks = quantity.getValue() / 3;
+        int singlePacks = quantity.getValue() % 3;
+
+        return Arrays.asList(
+                PricingPartition.builder()
+                        .article(article)
+                        .quantity(new Quantity(nbPacks))
+                        .pricingStrategy(this)
+                        .build(),
+                PricingPartition.builder()
+                        .article(article)
+                        .quantity(new Quantity(singlePacks))
+                        .pricingStrategy(DefaultByUnitPricing.create())
+                        .build()
+        );
     }
 }
