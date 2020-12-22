3e87bb284eb266bf8980d75bee71fd4d08e14947
belseuce <rufin.assoumou@gmail.com>
2020-12-22 21:12:15 +0100

    refactor bonus strategy tests

--- src/test/java/j/r/a/supermarket/strategy/impl/PackOfTwoBonusPricingTest.java
+++ src/test/java/j/r/a/supermarket/strategy/impl/PackOfTwoBonusPricingTest.java
@@ -9,24 +9,30 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 class PackOfTwoBonusPricingTest {
-    private final Article article = new Article("Orange", "fruit");
+    private final Article article = new Article("orange", "fruit");
 
     @Test
-    public void should_give_2_partitions_for_6_articles() {
-        IPricingStrategy bonusStrategy = new PackOfTwoBonusPricing();
-        PricingPartition bonusPartitions = PricingPartition.builder()
+    public void should_give_2_partitions_of_3_and_3_single_for_7_articles() {
+        IPricingStrategy tripletStrategy = new PackOfTwoBonusPricing();
+        IPricingStrategy singleStrategy = new DefaultByUnitPricing();
+        PricingPartition bonusPartition = PricingPartition.builder()
                 .article(article)
                 .quantity(new Quantity(2))
-                .pricingStrategy(bonusStrategy)
+                .pricingStrategy(tripletStrategy)
                 .build();
-        List<PricingPartition> pricingPartitions = Collections.singletonList(bonusPartitions);
-        Assert.assertTrue(pricingPartitions.stream().allMatch(pricingPart -> pricingPart.getPricingStrategy().equals(bonusStrategy)));
+        PricingPartition singlePartition = PricingPartition.builder()
+                .article(article)
+                .quantity(new Quantity(1))
+                .pricingStrategy(singleStrategy)
+                .build();
+        List<PricingPartition> pricingPartitions = Arrays.asList(bonusPartition, singlePartition);
+        Assert.assertTrue(pricingPartitions.stream().anyMatch(pricingPart -> pricingPart.getPricingStrategy().equals(tripletStrategy)));
+        Assert.assertTrue(pricingPartitions.stream().anyMatch(pricingPart -> pricingPart.getPricingStrategy().equals(singleStrategy)));
 
-        Assert.assertThat(bonusStrategy.getPricingPartitions(new Quantity(6), article), Is.is(pricingPartitions));
+        Assert.assertThat(tripletStrategy.getPricingPartitions(new Quantity(7), article), Is.is(pricingPartitions));
     }
 
 }
